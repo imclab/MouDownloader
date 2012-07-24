3d5eca253ad254ec967da4faceaba06a08891dea
Arnaud Aliès <arnaudalies.py@gmail.com>
2012-07-24 12:43:13 -1000

    Update api/modules.py
    +fast
    -code
    -bugs

--- api/modules.py
+++ api/modules.py
@@ -42,33 +42,24 @@ def GetPornhubVideoLink(link):
 def GetYoujizzVideoLink(link):
 	'''Get the direct link to the video, return 1 if any errors
 	don't forget the http://'''
-	before_video = '<div class="img">\r\n                \t<a href="'
-	after_video = '" target="_self"><img'
-	try:
-		page = urlopen(link).read()
-		#getting the page of the link
-	except (IOError):
-		return 1
-	
-	#searching the title
+	before_mobile_link = "/videos/"
 	before_title = "<title>"
 	after_title = "</title>"
-	real_title = page[page.find(before_title)+len(before_title):page.find(after_title)]
-	if (not len(real_title)):
-		#sometime the video page is loading with an empty title (searching the name again)
-		before_title = '<h2 style="margin: 5px;">'
-		after_title = '</h2>'
-		real_title = page[page.find(before_title)+len(before_title):page.find(after_title)]
-	#end of the search of the title of the video
-	title = real_title.replace(" ", "%20")
-	#during a search we need to replace ' ' with %20
+	before_direct_link = '<a class="preview_thumb" href="'
+	after_direct_link = '.mp4'
+	
+	mobile_link = link.replace("www", "m")
+	#transforming desktop link to mobile link
 	try:
-		search = urlopen("http://m.youjizz.com/search/%s/page1.html" % title).read()
-		#launch a search on the website
+		page = urlopen(mobile_link).read()
 	except (IOError):
 		return 1
-	if ("mp4" not in search):
-		return 1
-	#directly search and return the direct link after the search
-	video_link = search[search.find(before_video)+len(before_video):search.find(after_video)]
-	return [real_title, video_link]
+	#loading the page
+	
+	title = page[page.find(before_title)+len(before_title):page.find(after_title)]
+	#getting the title of the video
+	video_link = page[page.find(before_direct_link)+len(before_direct_link):page.find(after_direct_link)+len(after_direct_link)]
+	#getting the direct link to the video
+	
+	return [title, video_link]
+
